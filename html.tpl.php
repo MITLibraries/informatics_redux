a1fb883d4351688e1a07f5cfbcdcc107f91751b8
Matt Bernhardt <mjbernha@mit.edu>
2015-08-12 17:12:22 -0400

    Adds HTML page template for debugging

---
+++ html.tpl.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
+<head>
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<!--
+_<?php 
+global $base_url;
+echo 'Base URL: ' . $base_url ."\n";
+echo 'Adjusted PURL: ' . purl_get_normal_path("about") . "\n";
+echo 'Adjusted PURL: ' . purl_get_normal_path("node/473") . "\n";
+echo '_';
+print_r($options['purl']); 
+?>_
+-->
+<base href="http://projects.informatics-dev.mit.edu/fubar">
+<meta http-equiv="x-ua-compatible" content="IE=edge">
+  <?php if ($use_responsive_behaviors): ?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <?php else: ?>
+<meta name="viewport" content="width=1000" />
+  <?php endif; ?>
+<?php print $styles; ?>
+<?php print $scripts; ?>
+<?php print $polyfills; ?>
+  <!--[if lte IE 8]>
+<script type="text/javascript">
+  var os_c = document.createElement;os_c('header');os_c('nav');os_c('section');os_c('article');os_c('aside');os_c('footer');os_c('hgroup');os_c('figure');
+</script>
+  <![endif]-->
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div id="skip-link">
+    <a href="<?php print $skip_link_target; ?>" class="element-invisible element-focusable" tabindex="1"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
